26e4994558b9ce99d2cdaed5704e2b1b3589590a
Aaradhya Raut <aaradhya1402@gmail.com>
2026-02-23 06:36:34 +0000

    upd1 of w5

---
+++ ccl/exp4/1.cpp
@@ -0,0 +1,12 @@
+// 1. Check for balanced parentheses using a stack.
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> myStack;
+
+    cout << "hello world!\n";
+
+    return 0;
+}
